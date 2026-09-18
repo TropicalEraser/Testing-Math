58c39517a0ed5351c3214ad6b0c53060aba06cd0
TropicalEraser <128eyang@frhsd.com>
2026-09-18 12:43:53 -0400

    removed delta for testreciprocal

--- src/MathTest.java
+++ src/MathTest.java
@@ -39,7 +39,7 @@ public class MathTest {
 
     public void testReciprocal(double base, double exponent) {
 
-        assertEquals(1 / (Math.pow(base, -1 * exponent)), Math.pow(base, exponent), 0.000001);
+        assertEquals(1 / (Math.pow(base, -1 * exponent)), Math.pow(base, exponent));
         //x-a = 1/xa
 
 
