c86c569884460e3b869023ec02a683d4fbf40d16
TropicalEraser <128eyang@frhsd.com>
2026-09-18 12:37:58 -0400

    testzeropower

--- src/MathTest.java
+++ src/MathTest.java
@@ -105,7 +105,7 @@ public class MathTest {
             "-7.4, -8.3, -9.2"
     })
     public void testNestedPowers(double base, double a, double b) {
-        assertEquals(Math.pow(base, a * b), Math.pow(Math.pow(base, a), b));
+        assertEquals(Math.pow(base, a * b), Math.pow(Math.pow(base, a), b), 0.000001);
         //(xa)b = xa*b
     }
 
@@ -113,7 +113,7 @@ public class MathTest {
     @ParameterizedTest
     @ValueSource(doubles = {1, 5.0, 3.7, -1, -4.0, -4.9})
     public void testZeroPower(double base) {
-
+        assertEquals(1, Math.pow(base, 0));
         //x0 = 1
     }
 
