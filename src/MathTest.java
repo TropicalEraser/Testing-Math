3ad5b87ba4e597348d8f95d8b1b2e4912aebe74c
TropicalEraser <128eyang@frhsd.com>
2026-09-18 12:38:34 -0400

    testonepower

--- src/MathTest.java
+++ src/MathTest.java
@@ -121,6 +121,7 @@ public class MathTest {
     @ParameterizedTest
     @ValueSource(doubles = {1, 5.0, 3.7, -1, -4.0, -4.9})
     public void testOnePower(double base) {
+        assertEquals(base, Math.pow(base, 1));
         //x1 = x
     }
 
